298200ac40c1353a2008e83497135a3eca68a9e6
grpmart007-netizen <grpmart007@gmail.com>
2025-11-27 09:00:08 -0300

    atualizando exercicios

--- exercicios-lista/script.js
+++ exercicios-lista/script.js
@@ -1,6 +1,33 @@
-document.getElementById("todo-form")
-document.getElementById("task-input")
-document.getElementById("todo-list")
 
-addTask(event)
+let formulario = document.getElementById("todo-form")
+let input = document.getElementById("task-input")
+let lista = document.getElementById("todo-list")
 
+function adicionarTarefa(event){
+    let texto = input.value
+    if(texto != ""){
+        let novoitem = document.createElement("li")
+        novoitem.classList.add("todo-item")
+        novoitem.addEventListener("click", ()=>{
+            novoitem.classList.toggle("completed")
+        })
+
+        let textoitem = document.createElement("span")
+        textoitem.innerText = texto
+        novoitem.appendChild(textoitem
+
+        )
+        let botaodeletar = document.createElement("button")
+        botaodeletar.innerText = "Deletar"
+        botaodeletar.classList.add("btn-excluir")
+        botaodeletar.addEventListener("click", ()=>{
+            lista.replaceChild(novoitem)
+        })
+        novoitem.appendChild(botaodeletar)
+
+        lista.appendChild(novoitem)
+        input.value = ""
+    }
+}
+
+formulario.addEventListener("submit", adicionarTarefa)
