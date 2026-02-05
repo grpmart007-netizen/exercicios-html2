6dffd309d723dd9a28570ebee022fb6654a29df9
grpmart007-netizen <grpmart007@gmail.com>
2026-02-05 09:06:46 -0300

    ATUALIZANDO EXERCICIOS

---
+++ POO - EXERCICIOS/campeonato.js
@@ -0,0 +1,13 @@
+class time {
+    vitorias = 0
+    empates = 0
+    derrotas = 0
+    constructor(nome){
+        this.nome = nome
+    }
+    calcularPontos(pontos){
+    let pontos = this.vitorias * 3 + this.empates; this.derrotas * 0
+    return pontos
+   }  
+}
+
